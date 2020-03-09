9d86cf55357e77793c1e0eb561d6f3b88b09119d
Aysam Farhoud <aysamfar11@gmail.com>
2020-03-09 14:38:15 +0200

    designs landing page, more responsive and all components designed together

--- client/src/components/WebDescription/WebDescription.js
+++ client/src/components/WebDescription/WebDescription.js
@@ -7,13 +7,14 @@ const WebDescription = () => (
       <div className='description'>
         <div className='bg-image'></div>
         <div className='content'>
-        <h1>Your journey begins</h1>
-        <p>Build new skills with groups of self learners, join a group and be part of the community</p>
+          <h1>Your journey for learning</h1>
+          <p>Build new skills with groups of self learners, join a group and be part of the community</p>
         </div>
+        <span className="joinForFree">Join for free</span>
         <div className='description-under-header' >
           <h1>Find a suitable course for the subject you want, join a group and learn together!</h1>
+          <div className='speration-line'></div>
         </div>
-        <div className='speration-line'></div>
       </div>
     </div>
 );
