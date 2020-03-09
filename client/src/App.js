3aa4b50cf34a4c5ce3e6d7e935b19ef9ac62e9b0
Aysam Farhoud <aysamfar11@gmail.com>
2020-03-09 16:47:52 +0200

    adds course page and react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 import './App.css';
 import Header from './components/Header/Header';
 import WebDescription from './components/WebDescription/WebDescription'
 import OurServices from './components/OurServices/OurServices'
+import CoursePage from './pages/CoursePage'
 
 function App() {
   return (
     <div>
+    <Router>
     <Header/>
-    <WebDescription/>
-    <OurServices/>
+          <Switch>
+            <Route path="/coursePage">
+              <CoursePage />
+            </Route>
+            <Route path="/">
+              <WebDescription/>
+              <OurServices/>
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
