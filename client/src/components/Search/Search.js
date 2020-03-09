a7f53d8a67f781e9b10b4b49411ad3cb20438f40
LogicX1 <uninajone@gmail.com>
2020-03-09 23:31:58 +0200

    Fixed some issues as Mario suggeste,no more <br>,formatted code,moved dummy courses to app.js,removed dublicate css from Search.css

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -2,18 +2,14 @@ import './Search.css';
 import React from 'react';
 import SearchResults from '../SearchResults/SearchResults';
 import Spinner from '../Spinner/Spinner';
-import get from '../../../src/utils/axiosGet'
+import get from '../../../src/utils/axiosGet';
 const Search = () => {
-const [error,setError] = React.useState(null);
-const [loading,setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
 
-if (loading) {
-return Spinner();
-}
-
-
-
-}
-
+  if (loading) {
+    return Spinner();
+  }
+};
 
 export default Search;
--- client/src/components/Spinner/Spinner.js
+++ client/src/components/Spinner/Spinner.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React from 'react';
 import './Spinner.css';
 const Spinner = () => (
   <div className='spinner-container'>
-  <div className='loader'></div>
+      <div className='loader'></div>
   </div>
 );
-
 
 export default Spinner;
