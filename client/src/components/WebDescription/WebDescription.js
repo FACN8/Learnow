081a245f86020dc7ec10dd4598028a0a5d58039b
LogicX1 <uninajone@gmail.com>
2020-03-09 10:12:48 +0200

    Fixing up some css problems when we joined the componet with header

--- client/src/components/WebDescription/WebDescription.js
+++ client/src/components/WebDescription/WebDescription.js
@@ -6,6 +6,10 @@ const WebDescription = () => (
     <div className='container'>
       <div className='description'>
         <div className='bg-image'></div>
+        <div className='content'>
+        <h1>Your journey begins</h1>
+        <p>Build new skills with groups of self learners, join a group and be part of the community</p>
+        </div>
       </div>
     </div>
 );
