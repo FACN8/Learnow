ec78b19538df1f291d512c5ee443d34c7723673a
Aysam Farhoud <aysamfar11@gmail.com>
2020-03-10 17:44:37 +0200

    naming the folders and files

--- client/src/pages/CoursePage.js
+++ client/src/pages/CoursePage.js
@@ -1,15 +1,14 @@
 import React from 'react';
-//import './coursePage.css';
-import SelectedCoursePage from '../components/SelectedCoursePage/SelectedCoursePage';
+import SelectedCourse from '../components/SelectedCourse/SelectedCourse';
 
 
 function CoursePage() {
   return (
     <div>
-    <SelectedCoursePage id = {1} title = {"Algebra I"}
-      headline = {"The entry course to learn algebra"}
-      url =  {"https://www.udemy.com/course/algebra-i-beginning-algebra/"}
-      image = {"https://i.udemycdn.com/course/240x135/34267_f952_15.jpg"}
+    <SelectedCourse id = {1} title = "Algebra I"
+      headline = "The entry course to learn algebra"
+      url =  "https://www.udemy.com/course/algebra-i-beginning-algebra/"
+      image = "https://i.udemycdn.com/course/240x135/34267_f952_15.jpg"
       num_lectures = {14} num_subscribers = {566}
     />
     </div>
