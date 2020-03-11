a7c66539e368e5755ed0ea41f08a4b5f18b26b61
Aysam Farhoud <aysamfar11@gmail.com>
2020-03-11 17:20:29 +0200

    adds back button

--- client/src/pages/CoursePage.js
+++ client/src/pages/CoursePage.js
@@ -8,12 +8,7 @@ function CoursePage({ state, setState }) {
   console.log(course);
   return (
     <div>
-    <SelectedCourse id = {1} title = {course.title}
-      headline = {course.headline}
-      url = {course.url}
-      image = {course.image_240x135}
-
-      num_lectures = {14} num_subscribers = {566}
+    <SelectedCourse 
       state={state}
       setState={setState}
     />
