1875e3e51a54c5f40fc2a9b489ae82780be2b796
LogicX1 <uninajone@gmail.com>
2020-03-11 16:20:06 +0200

    Worked on navigating from the results to a specific course page and back

--- client/src/components/SearchResults/SearchResults.js
+++ client/src/components/SearchResults/SearchResults.js
@@ -2,6 +2,7 @@
 import './SearchResults.css';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import CoursePage from '../../pages/CoursePage';
 
 
 const SearchResults = ({ time, data }) => {
@@ -23,21 +24,21 @@ const SearchResults = ({ time, data }) => {
 
   const results = courses.map(course => (
     <li key={course.id}>
-        <img onClick={() => setState(prevState => ({ ...prevState, selectedCourse: course.id }))} src={course.image_240x135} />
+        <img alt='course-image' src={course.image_240x135} />
       <div className='description'>
           <h2 >{course.title}</h2>
         <h3>{course.headline}</h3>
         <span className='statistics'>
           Lectures: {course.num_lectures} ·· Students: {course.num_subscribers}
         </span>
-        <a href={course.url}>
-          <span className='udemy-link'>View on udemy</span>
-        </a>
+          <span onClick={() => setState(prevState => ({ ...prevState, selectedCourse: course }))} className='udemy-link'>More details</span>
       </div>
     </li>
   ));
 
-
+  if (state.selectedCourse) {
+    return <CoursePage state={state} setState={setState} />;
+  }
   return (
     <div className='results-container'>
       <span className='results-count'>{`${data.count} results found in ${time / 1000} Seconds`}</span>
--- client/src/components/SelectedCourse/SelectedCourse.js
+++ client/src/components/SelectedCourse/SelectedCourse.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React from 'react';
 import './SelectedCourse.css';
 
 import {
-  Link
-} from "react-router-dom";
+  Link,
+} from 'react-router-dom';
 
-function SelectedCourse({id, title, headline, url, image,
- num_lectures, num_subscribers}){
+function SelectedCourse({
+  id, title, headline, url, image,
+  num_lectures, num_subscribers, state, setState,
+}) {
+  window.onbeforeunload = () => {
+    console.log('backed');
+    setState({ ...state, selectedCourse: null });
+  };
   return (
     <div>
       <div className="greyBackGround">
@@ -14,10 +20,11 @@ function SelectedCourse({id, title, headline, url, image,
         <h3 className="courseDesc">{headline}</h3>
         <h4 className="courseLects">{num_lectures} lectures for this course</h4>
         <h4 className="courseSubs">{num_subscribers} students enrolled in this course</h4>
-        <h4 className="courseURL">Visit course page on udemy <a style={{"color": "wheat "}} href={url}>HERE</a>.</h4>
-        <img className="courseImage" src={image}></img>
+        <h4 className="courseURL">Visit course page on udemy <a style={{ color: 'wheat ' }} href={url}>HERE</a>.</h4>
+        <img className="courseImage" src={image}/>
+        <h3 onClick={() => setState({ ...state, selectedCourse: null })}>Return to result</h3>
       </div>
-      
+
     </div>
   );
 }
