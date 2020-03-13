c7a1cdf600319581b3136e9896880402f41574a5
LogicX1 <uninajone@gmail.com>
2020-03-13 13:51:38 +0200

    added some query validations on backend

--- server/app/actions/getCourses.js
+++ server/app/actions/getCourses.js
@@ -5,9 +5,26 @@ const axiosGet = require('../util/axiosGet');
 
 // `https://www.udemy.com/api-2.0/courses/?search=${term}&page=${page}&page_size=50`;
 
+function validateQuery(query) {
+  const {search,page,page_size} = query;
+if( +page<1 ) return false;
+if(+page_size < 1 || +page_size >50) return false ;
+return true;
+}
+
 
   module.exports = (req,res) => {
+    if(!validateQuery(req.query)){
+      res.status(400).send({
+        message:'Invalid queries'
+      })
+    }
     const {search,page,page_size} = req.query;
+    console.log(`Page type is : ${typeof(page)}`);
+    console.log(`Page_size type is : ${typeof(page_size)}`);
+    console.log(`Search term type is : ${typeof(search)}`);
+
+
     const apiUrl = `https://www.udemy.com/api-2.0/courses/?search=${search}&page=${page}  &page_size=${page_size}`;
     axiosGet(apiUrl)
     .then(result => {
