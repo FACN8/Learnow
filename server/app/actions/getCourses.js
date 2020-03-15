000eb2f0c01e439f776fa676bcca8e0e93ed632c
LogicX1 <uninajone@gmail.com>
2020-03-15 14:16:11 +0200

    The request is not working on heroku Although locally its working, on heroku it is not, small changes done,the api link had extra spaces that are removed

--- server/app/actions/getCourses.js
+++ server/app/actions/getCourses.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 const axiosGet = require('../util/axiosGet');
 
-//http://localhost:5000/getList/https://www.udemy.com/api-2.0/courses/?search=algebra&page=1&page_size=50
 
-// `https://www.udemy.com/api-2.0/courses/?search=${term}&page=${page}&page_size=50`;
 
 function validateQuery(query) {
   const {search,page,page_size} = query;
@@ -14,18 +12,16 @@ return true;
 
 
   module.exports = (req,res) => {
+
     if(!validateQuery(req.query)){
       res.status(400).send({
         message:'Invalid queries'
       })
     }
     const {search,page,page_size} = req.query;
-    console.log(`Page type is : ${typeof(page)}`);
-    console.log(`Page_size type is : ${typeof(page_size)}`);
-    console.log(`Search term type is : ${typeof(search)}`);
 
 
-    const apiUrl = `https://www.udemy.com/api-2.0/courses/?search=${search}&page=${page}  &page_size=${page_size}`;
+    const apiUrl = `https://www.udemy.com/api-2.0/courses/?search=${search}&page=${page}&page_size=${page_size}`;
     axiosGet(apiUrl)
     .then(result => {
     res.json(result.data);
@@ -42,4 +38,3 @@ return true;
 
 
 
-//     const URL = `https://cors-anywhere.herokuapp.com/${apiBase}/courses/?search=${term}&page=${page}&page_size=50`;
