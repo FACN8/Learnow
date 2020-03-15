210cabdf76c00d1cccdab239dc34d591c9d37163
Aysam Farhoud <aysamfar11@gmail.com>
2020-03-15 20:22:51 +0200

    initial chat socket io

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,7 +3,11 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const middlewares = require('../middlewares/middlewares');
- 
+//const app = require("./app");
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const connectedUsers = [];
+
 
 app.use(morgan('common'));
 
@@ -15,8 +19,46 @@ app.use(
   }),
 );
 
+io.on("connection", socket => {
+  var userName = "";
+  console.log("ENTERRRRRRRRRRRRRRRRRRRRRRRRR");
+  socket.on("user connected", newUser => {
+    console.log("New user connected to backend", newUser);
+    userName = newUser;
+    connectedUsers.push(userName);
+    console.log(`${userName} has joind the game`);
+    console.log(`Connected users :`);
+    console.table(connectedUsers);
+    io.emit("update connected users", connectedUsers);
+
+
+    socket.on("chat message", function(msg) {
+      console.log(msg);
+      io.emit("chat message", userName + " :" + msg);
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`${userName} has left`);
+    const userIndex = connectedUsers.indexOf(userName);
+    connectedUsers.splice(userIndex, 1);
+    console.log(`Connected users :`);
+    console.table(connectedUsers);
+    if(connectedUsers.length<2){
+      gameStatus=gamePhases.ended;
+      io.emit("round end",['Game canceled no']);
+    }
+    io.emit("update connected users", connectedUsers);
+  });
+});
+
+
 app.get('/', (req, res) => {
   res.send('Welcome home');
+});
+
+app.get('/GroupChat', function(req, res){
+  res.sendFile(__dirname +'/GroupChat/GroupChat.html');
 });
 
 app.get('/getcourses/*',require('../actions/getCourses'));
