0c7bf69e8f5657310be3b1b2d542e08b33246bc2
LogicX1 <uninajone@gmail.com>
2020-03-15 20:07:30 +0200

    Initial work on the backend for auth rel #42 Initial setup to check and authenticate a user based on the token,did not add the routes yet

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,7 +3,23 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const middlewares = require('../middlewares/middlewares');
- 
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+const jwtAuthz = require('express-jwt-authz');
+require ('dotenv').config();
+var jwtCheck = jwt({
+      secret: jwks.expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+    }),
+    audience: `https://${process.env.AUTH0_AUDIENCE}/`,
+    issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+    algorithms: ['RS256']
+});
+
+
 
 app.use(morgan('common'));
 
@@ -15,9 +31,17 @@ app.use(
   }),
 );
 
+app.use(jwtCheck);
+
+app.get('/authorized', function (req, res) {
+    res.send('Secured Resource');
+});
+
+
 app.get('/', (req, res) => {
   res.send('Welcome home');
 });
+
 
 app.get('/getcourses/*',require('../actions/getCourses'));
 
