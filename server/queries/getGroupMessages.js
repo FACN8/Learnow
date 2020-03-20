60ac32db2140def2e2d6acd9894b69ab81a3dadf
logicx1 <uninajone@gmail.com>
2020-03-20 02:02:56 +0200

    Added user_id to be retrived with the messages

--- server/queries/getGroupMessages.js
+++ server/queries/getGroupMessages.js
@@ -3,7 +3,7 @@ const dbConnection = require('../database/db_connection');
 module.exports = (groupId, cb) => {
   dbConnection.query(
     `
-    SELECT user_name, message, updated_at 
+    SELECT user_name,user_id, message, updated_at 
     FROM messages
     WHERE group_id = $1
     `,
