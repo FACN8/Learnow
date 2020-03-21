50e29bcdaf987755a4ca26da3cb28ff69d17c0d7
logicx1 <uninajone@gmail.com>
2020-03-22 00:26:42 +0200

    Fixing a weird thing where this file remained as an old version when i made this new branch

--- client/src/components/GroupChat/GroupChat.js
+++ client/src/components/GroupChat/GroupChat.js
@@ -2,15 +2,16 @@ import React, { useEffect, useState } from 'react';
 import './GroupChat.css';
 import io from 'socket.io-client';
 
-function GroupChat() {
+function GroupChat({ group, setGroup, user }) {
   let apiUrl = 'http://localhost:5000';
   if (process.env.NODE_ENV === 'production') {
     apiUrl = 'https://learnow-be.herokuapp.com';
   }
+
   const [socket, setSocket] = useState(io.connect(apiUrl));
   const [msgsArray, setMsgsArray] = useState([]);
   useEffect(() => {
-    socket.emit('user connected', 'Aysam1');
+    socket.emit('user connected', user.nickname);
   }, []);
 
   const sendMsg = e => {
@@ -21,20 +22,46 @@ function GroupChat() {
   socket.on('chat message', function(msg) {
     setMsgsArray([...msgsArray, msg]);
   });
+
+  console.log(user);
   return (
     <div>
-      <ul id='messages'></ul>
-      {msgsArray.map(message => (
-        <li>{message}</li>
-      ))}
-      <form className='chat-from' onSubmit={sendMsg}>
-        <label htmlFor='msg'>
-          <input type='text' name='msg' id='m' autoComplete='off' />
-        </label>
-        <button type='submit' value='Send'>
-          Send
-        </button>
-      </form>
+      <div className='groupchat-nav'>
+        <span className='back-to-groups' onClick={() => setGroup(null)}>
+          ⟵ Back to groups
+        </span>
+        <div className='groupname-container'>
+          <span className='chat-groupname' onClick={() => setGroup(null)}>
+            {group.name}
+          </span>
+        </div>
+      </div>
+      <div className='chat-container'>
+        <div className='users-and-msgs-container'>
+          <ul className='users-container'></ul>
+          <div className='message-box'>
+            <ul className='messages'>
+              {msgsArray.map(message => (
+                <li className='message-listing'>
+                  <img src={user.picture} className='mini-user-pic' />
+                  <div className='sent-msg-container'>
+                    <h4>{user.nickname}</h4>
+                    <span className='sent-msg'>{message.split(':')[1]}</span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <form className='chat-from' onSubmit={sendMsg}>
+              <label className='input-label' htmlFor='msg'>
+                <input type='text' name='msg' id='m' autoComplete='off' />
+              </label>
+              <button type='submit' value='Send'>
+                Send
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
