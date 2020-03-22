78ec4b4630c2a79d6e8065fbf8fca574a46b9bf5
logicx1 <uninajone@gmail.com>
2020-03-22 05:34:10 +0200

    didnt work

--- client/src/components/UsersList/UsersList.js
+++ client/src/components/UsersList/UsersList.js
@@ -3,30 +3,32 @@ import axiosGet from '../../utils/axiosGet';
 import './UsersList.css';
 
 const UsersList = ({ groupId, socket }) => {
-
   const [usersArray, setUsersArray] = useState([]);
   const [connectedUsers, setConnectedUsers] = useState([]);
-  const [update,setUpdate] = useState(false);
+  
   socket.on('update connected users', users => {
     setConnectedUsers(...users);
-    setUpdate((update)=>!update);
-    console.log(users);
   });
 
-  // const status = userName => {
-  //   let classes = 'status';
-    
-  //   if(connectedUsers){
-  //     if (connectedUsers.includes(userName)) classes += ' online';
-  //   }
+  useEffect(()=>{
+    if (connectedUsers.length && usersArray.length) {
+      usersArray.sort(curr => {
+         return connectedUsers.includes(curr) ? 1 : -1;
+       });
+     }
+  },[connectedUsers])
 
-  //   return classes;
-  // };
+  const status = userName => {
+    let classes = 'status';
+    if (connectedUsers) {
+      if (connectedUsers.includes(userName)) classes += ' online';
+    }
+    return classes;
+  };
 
   const getUsers = () => {
     axiosGet(`/users/get/groupid=${groupId}`)
       .then(res => {
-        res.data.sort((curr)=>connectedUsers.indexOf(curr))
         setUsersArray(res.data);
       })
       .catch(err => setUsersArray(['Could not fetch users!']));
@@ -41,19 +43,14 @@ const UsersList = ({ groupId, socket }) => {
   return (
     <ul className='users-list'>
       {usersArray.map(user => {
-        let status ='status';
-        if(connectedUsers.includes(user.user_name))
-        status+=' online';
         return (
-        <li key={user.user_id} className='user-listing'>
-          <img src={user.picture} className='mini-user-pic' />
-          <h3 className='user-in-list'>{user.user_name}</h3>
-          <div className={status}></div>
-        </li>
-        )
-      })
-      
-    }
+          <li key={user.user_id} className='user-listing'>
+            <img src={user.picture} className='mini-user-pic' />
+            <h3 className='user-in-list'>{user.user_name}</h3>
+            <div className={status(user.user_name)}></div>
+          </li>
+        );
+      })}
     </ul>
   );
 };
